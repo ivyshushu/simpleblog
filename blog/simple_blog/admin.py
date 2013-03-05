9c0d847801f0d1bb02092af3aba500516ad2ba90
ivyshushu <ivyshushu@gmail.com>
2013-03-05 15:28:58 -0800

    continue...

---
+++ blog/simple_blog/admin.py
@@ -0,0 +1,8 @@
+###Admin
+from simple_blog.models import Post
+from django.contrib import admin
+
+class PostAdmin(admin.ModelAdmin):
+	search_fields = ["title"]
+	
+admin.site.register(Post, PostAdmin)
--- blog/simple_blog/views.py
+++ blog/simple_blog/views.py
@@ -1,1 +1,24 @@
 # Create your views here.
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.urlresolvers import reverse
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
+
+
+from simple_blog.models import *
+
+def main(request):
+    """Main listing."""
+    posts = Post.objects.all().order_by("-created")
+    paginator = Paginator(posts, 2)
+
+    try: page = int(request.GET.get("page", '1'))
+    except ValueError: page = 1
+
+    try:
+        posts = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        posts = paginator.page(paginator.num_pages)
+
+    return render_to_response("blog/list.html", dict(posts=posts, user=request.user))
